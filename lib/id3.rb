65ab1e4a68c44fc7eedd419ed982d33888bd982c
Jeremy Burks <jeremy.burks@gmail.com>
2008-12-19 15:49:48 -0600

    use thor for command line parsing

--- lib/id3.rb
+++ lib/id3.rb
@@ -3,6 +3,7 @@ $:.unshift File.expand_path(File.dirname(__FILE__))
 require 'rubygems'
 require 'extlib'
 require 'extlib/dictionary'
+require 'thor'
 require 'id3lib'
 
 require 'id3/tag_bag/anonymizer'
