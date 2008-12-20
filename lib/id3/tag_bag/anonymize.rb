9cb14d184beb1b556549595d43b13be7faa6af17
Jeremy Burks <jeremy.burks@gmail.com>
2008-12-19 22:09:21 -0600

    added --dry-run option to anonymize, renamed it to be consistent with inspect

--- lib/id3/tag_bag/anonymize.rb
+++ lib/id3/tag_bag/anonymize.rb
@@ -1,22 +1,22 @@
 module ID3
   module TagBag
     
-    class Anonymizer < Base
+    class Anonymize < Base
       FrameIds = [:COMM, :TCOP, :TENC, :TPUB, :USER]
       
-      def run
+      def run(dry_run)
         each do |tag|
           update = false
           FrameIds.each do |id|
             unless (text = tag.frame_text(id)).nil?
-              tag.set_frame_text(id, nil)
+              tag.set_frame_text(id, nil) unless dry_run
               puts %Q[#{id} - Removed "#{text}"]
               update = true
             end
           end
           next unless update
           begin
-            tag.update!
+            tag.update! unless dry_run
             puts "Updated #{tag.filename}"
           rescue => e
             puts "Failed to update #{tag.filename}: #{e.message}"
